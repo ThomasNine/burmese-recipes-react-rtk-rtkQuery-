28bb47de0f16b27b8789959bc160251a4a264ce6
ThomasNine <thomasnine48@gmail.com>
2024-03-08 18:09:54 +0630

    fixed search icon

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -57,24 +57,23 @@ export default function NavbarComponent() {
       <NavbarContent className="flex gap-1" justify="end">
         <NavbarItem>
           <NavLink to={"/search/page/1"}>
-            <Tooltip content={lang === "eng" ? "Search" : "ရှာဖွေပါ"}>
-              <Button isIconOnly size="sm" color="default" variant="bordered">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-5 h-5"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-                  />
-                </svg>
-              </Button>
-            </Tooltip>
+            {/* <Tooltip content={lang === "eng" ? "Search" : "ရှာဖွေပါ"}></Tooltip> */}
+            <Button isIconOnly size="sm" color="default" variant="bordered">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                />
+              </svg>
+            </Button>
           </NavLink>
         </NavbarItem>
         <NavbarItem>
