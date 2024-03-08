ad47398f996b2b5fb92941308e2c91bda8e7903a
ThomasNine <thomasnine48@gmail.com>
2024-03-08 10:59:45 +0630

    prepare for vercel.json

--- src/store/services/ApiService.js
+++ src/store/services/ApiService.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = import.meta.env.VITE_API_BASE_URL || "";
+const baseUrl =
+  import.meta.env.VITE_API_BASE_URL || "https://burmese-recipes-rtk.vercel.app";
 
 export const ApiService = createApi({
   reducerPath: "api",
